7ca0976009c2218071b6fd7f01547005d453231a
simplyyliam <simplyliam.dev@gmail.com>
2025-07-10 21:07:19 +0200

    fix: correct NavLink active state styling and improve navbar component structure

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <div className="flex items-center justify-center w-full">
         <ModularNav>
-          <NavLink className={`${activeNav === "Boards" ? "text-white bg-[#FFAC4D]" : "text-black"}`} onClick={() => {
-            setActiveNav("Baords")
+          <NavLink className={`${activeNav === "Boards" ? "text-white bg-[#FFAC4D] shadow-[0px_4px_15px_-3px_#FFAC40]" : "text-black"}`} onClick={() => {
+            setActiveNav("Boards")
           }}>Boards</NavLink>
-          <NavLink className={`${activeNav === "All" ? "text-white bg-[#FFAC4D]" : "text-black"}`} onClick={() => {
+          <NavLink className={`${activeNav === "All" ? "text-white bg-[#FFAC4D] shadow-[0px_4px_15px_-3px_#FFAC40]" : "text-black"}`} onClick={() => {
             setActiveNav("All")
           }}>All</NavLink>
         </ModularNav>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import type { HtmlHTMLAttributes } from "react";
+import type { HtmlHTMLAttributes, ButtonHTMLAttributes } from "react";
 import React from "react";
 
 export const ModularNav: React.FC<HtmlHTMLAttributes<HTMLDivElement>> = ({
@@ -7,16 +7,16 @@ export const ModularNav: React.FC<HtmlHTMLAttributes<HTMLDivElement>> = ({
     ...props
 }) => {
     return (
-        <div className={`flex items-center justify-between gap-4 p-1 bg-neutral-200 rounded-full ${className}`} {...props}>{children}</div>
+        <div className={`flex items-center justify-between gap-4 p-1 bg-neutral-200 rounded-full border-2 border-[#ECECEC] shadow-[inset_0_4px_7px_-3px_#01010125] ${className}`} {...props}>{children}</div>
     )
 }
 
-export const NavLink: React.FC<HtmlHTMLAttributes<HTMLButtonElement>> = ({
+export const NavLink: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
     children,
     className,
     ...props
 }) => {
     return (
-        <button className={`flex items-center justify-center w-[71px] p-3 rounded-full ${className}`} {...props}>{children}</button>
+        <button className={`flex items-center justify-center w-[71px] p-3 rounded-full cursor-pointer hover:shadow-[0px_4px_15px_-3px_#FFAC40] hover:bg-[#FFAC40] hover:text-white transition ease-linear ${className}`} {...props}>{children}</button>
     )
 }
